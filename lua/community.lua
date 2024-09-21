563195be1e5daba2abd4a01e7b60fa52d9635b25
Jens Blawatt <jens.blawatt@codecentric.de>
2024-09-21 22:13:10 +0200

    my first config

---
+++ lua/community.lua
@@ -0,0 +1,34 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.xml" },
+  { import = "astrocommunity.colorscheme.helix-nvim" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+  { import = "astrocommunity.colorscheme.cyberdream-nvim" },
+  { import = "astrocommunity.completion.avante-nvim" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.editing-support.copilotchat-nvim" },
+  { import = "astrocommunity.editing-support.nvim-treesitter-context" },
+  { import = "astrocommunity.editing-support.undotree" },
+  { import = "astrocommunity.file-explorer.oil-nvim" },
+  { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
+  { import = "astrocommunity.git.neogit" },
+  { import = "astrocommunity.note-taking.obsidian-nvim" },
+  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.test.vim-test" },
+}
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+if true then return {} end
+
+return {
+  { "LuaSnip", enabled = false },
+  { "comp_luasnip", enabled = false },
+}
